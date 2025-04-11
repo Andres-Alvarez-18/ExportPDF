84c7540050329a487f3f4490ef2951ed0a25cbd5
Desarrollo Qdata <desarrolloqdata@outlook.com>
2025-04-11 18:50:22 -0500

    se ajusto para web

--- exporttopdf/src/app/api/export/route.js
+++ exporttopdf/src/app/api/export/route.js
@@ -1,19 +1,20 @@
-import puppeteer from "puppeteer";
+import chromium from 'chrome-aws-lambda';
 
 export async function POST(request) {
   try {
     const html = await request.text();
 
-    const browser = await puppeteer.launch({
-      headless: 'new',
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    const browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      executablePath: await chromium.executablePath || process.env.CHROME_EXECUTABLE_PATH,
+      headless: chromium.headless,
     });
 
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'networkidle0' });
 
     const pdfBuffer = await page.pdf({
-      format: 'A3',
+      format: 'A4',
       printBackground: true,
       margin: {
         top: '5mm',
@@ -21,19 +22,18 @@ export async function POST(request) {
         bottom: '5mm',
         left: '5mm',
       },
-      scale: 1,
     });
 
     await browser.close();
 
     const base64String = pdfBuffer.toString('base64');
-
     return new Response(base64String, {
       status: 200,
       headers: { 'Content-Type': 'text/plain' },
     });
+
   } catch (err) {
-    console.error("Error generando PDF:", err);
+    console.error('Error generando PDF:', err);
     return new Response('Error generando el PDF', { status: 500 });
   }
 }
